3f7b1a76f42913e9a8838cce97551e023c79e05c
joseagustian <joseagustian37@gmail.com>
2023-02-23 23:36:47 +0700

    Fix host

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ async function init() {
 
     const server = hapi.server({
         port: 5000,
-        host: 'localhost',
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         routes: {
             cors: {
                 origin: ['*'],
